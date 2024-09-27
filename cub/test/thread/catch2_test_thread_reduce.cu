fa592ed6ab4fba50df250b6e06b4dd57b02eb94b
fbusato <fbusato@nvidia.com>
2024-09-27 19:36:01 +0000

    Add SASS checking macro

--- cub/test/thread/catch2_test_thread_reduce.cu
+++ cub/test/thread/catch2_test_thread_reduce.cu
@@ -35,6 +35,7 @@
 #include <cuda/std/limits>
 #include <cuda/std/type_traits>
 
+#include <cstring>
 #include <functional>
 #include <limits>
 #include <numeric>
@@ -47,15 +48,26 @@
 #include "c2h/generators.cuh"
 #include "catch2_test_helper.h"
 
+/***********************************************************************************************************************
+ * Thread Reduce Wrapper Kernels
+ **********************************************************************************************************************/
+
+//#define CCCL_CHECK_SASS // instantiate only the kernels useful for SASS inspection
+
 template <int NUM_ITEMS, typename T, typename ReduceOperator>
-__global__ void thread_reduce_kernel(const T* d_in, T* d_out, ReduceOperator reduce_operator)
+__global__ void thread_reduce_kernel(const T* __restrict__ d_in, T* __restrict__ d_out, ReduceOperator reduce_operator)
 {
   T thread_data[NUM_ITEMS];
-#pragma unroll
+#if defined(CCCL_CHECK_SASS)
+  auto d_in_aligned = static_cast<T*>(__builtin_assume_aligned(d_in, (sizeof(T) < 4) ? 4 : sizeof(T)));
+  ::memcpy(thread_data, d_in_aligned, sizeof(thread_data));
+#else
+#  pragma unroll
   for (int i = 0; i < NUM_ITEMS; ++i)
   {
     thread_data[i] = d_in[i];
   }
+#endif
   *d_out = cub::ThreadReduce(thread_data, reduce_operator);
 }
 
@@ -189,7 +201,7 @@ struct cub_operator_to_identity<T, cub::Sum>
   static constexpr T value()
   {
     return T{};
-  };
+  }
 };
 
 template <typename T>
@@ -198,7 +210,7 @@ struct cub_operator_to_identity<T, cub::Mul>
   static constexpr T value()
   {
     return T{1};
-  };
+  }
 };
 
 template <typename T>
@@ -207,7 +219,7 @@ struct cub_operator_to_identity<T, cub::BitAnd>
   static constexpr T value()
   {
     return ~T{0};
-  };
+  }
 };
 
 template <typename T>
@@ -216,7 +228,7 @@ struct cub_operator_to_identity<T, cub::BitOr>
   static constexpr T value()
   {
     return T{0};
-  };
+  }
 };
 
 template <typename T>
@@ -225,7 +237,7 @@ struct cub_operator_to_identity<T, cub::BitXor>
   static constexpr T value()
   {
     return T{0};
-  };
+  }
 };
 
 template <typename T>
@@ -234,7 +246,7 @@ struct cub_operator_to_identity<T, cub::Min>
   static constexpr T value()
   {
     return ::std::numeric_limits<T>::max();
-  };
+  }
 };
 
 template <typename T>
@@ -243,7 +255,7 @@ struct cub_operator_to_identity<T, cub::Max>
   static constexpr T value()
   {
     return ::std::numeric_limits<T>::min();
-  };
+  }
 };
 
 /***********************************************************************************************************************
@@ -259,7 +271,13 @@ using narrow_precision_type_list = c2h::type_list<
 #endif
   >;
 
-using fp_type_list = c2h::type_list<float, double>;
+using fp_type_list =
+  c2h::type_list<float
+#if !defined(CCCL_CHECK_SASS)
+                 ,
+                 double
+#endif
+                 >;
 
 using integral_type_list = c2h::
   type_list<::cuda::std::int8_t, ::cuda::std::int16_t, ::cuda::std::uint16_t, ::cuda::std::int32_t, ::cuda::std::int64_t>;
@@ -272,7 +290,7 @@ using cub_operator_fp_list = c2h::type_list<cub::Sum, cub::Mul, cub::Min, cub::M
 /***********************************************************************************************************************
  * Verify results and kernel launch
  **********************************************************************************************************************/
-#define CCCCL_CHECK_MAX_NUM_ITEMS
+
 template <typename T, _CUB_TEMPLATE_REQUIRES(::cuda::std::is_floating_point<T>::value)>
 void verify_results(const T& expected_data, const T& test_results)
 {
@@ -291,7 +309,7 @@ void run_thread_reduce_kernel(
 {
   switch (num_items)
   {
-#if !defined(CCCCL_CHECK_MAX_NUM_ITEMS)
+#if !defined(CCCL_CHECK_SASS)
     case 1:
       thread_reduce_kernel<1>
         <<<1, 1>>>(thrust::raw_pointer_cast(in.data()), thrust::raw_pointer_cast(out.data()), reduce_operator);
@@ -352,11 +370,16 @@ void run_thread_reduce_kernel(
       thread_reduce_kernel<15>
         <<<1, 1>>>(thrust::raw_pointer_cast(in.data()), thrust::raw_pointer_cast(out.data()), reduce_operator);
       break;
-#endif
     case 16:
       thread_reduce_kernel<16>
         <<<1, 1>>>(thrust::raw_pointer_cast(in.data()), thrust::raw_pointer_cast(out.data()), reduce_operator);
       break;
+#else
+    case 18:
+      thread_reduce_kernel<18>
+        <<<1, 1>>>(thrust::raw_pointer_cast(in.data()), thrust::raw_pointer_cast(out.data()), reduce_operator);
+      break;
+#endif
     default:
       FAIL("Unsupported number of items");
   }
@@ -364,13 +387,13 @@ void run_thread_reduce_kernel(
   REQUIRE(cudaSuccess == cudaDeviceSynchronize());
 }
 
-#if !defined(CCCCL_CHECK_MAX_NUM_ITEMS)
+#if !defined(CCCL_CHECK_SASS)
 constexpr int min_size  = 1;
 constexpr int max_size  = 16;
 constexpr int num_seeds = 10;
 #else
-constexpr int min_size  = 16;
-constexpr int max_size  = 16;
+constexpr int min_size  = 18;
+constexpr int max_size  = 18;
 constexpr int num_seeds = 1;
 #endif
 
@@ -443,6 +466,8 @@ CUB_TEST("ThreadReduce Narrow PrecisionType Tests",
 
 #endif // defined(TEST_HALF_T) || defined(TEST_BF_T)
 
+#if defined(CCCL_CHECK_SASS)
+
 CUB_TEST("ThreadReduce Containter Tests", "[reduce][thread]")
 {
   c2h::device_vector<int> d_in(max_size);
@@ -457,19 +482,21 @@ CUB_TEST("ThreadReduce Containter Tests", "[reduce][thread]")
   REQUIRE(cudaSuccess == cudaDeviceSynchronize());
   verify_results(reference_result, c2h::host_vector<int>(d_out)[0]);
 
-#if _CCCL_STD_VER >= 2014
+#  if _CCCL_STD_VER >= 2014
   thread_reduce_kernel_span<max_size>
     <<<1, 1>>>(thrust::raw_pointer_cast(d_in.data()), thrust::raw_pointer_cast(d_out.data()), cub::Sum{});
   REQUIRE(cudaSuccess == cudaPeekAtLastError());
   REQUIRE(cudaSuccess == cudaDeviceSynchronize());
   verify_results(reference_result, c2h::host_vector<int>(d_out)[0]);
-#endif // _CCCL_STD_VER >= 2014
+#  endif // _CCCL_STD_VER >= 2014
 
-#if _CCCL_STD_VER >= 2023
+#  if _CCCL_STD_VER >= 2023
   thread_reduce_kernel_mdspan<max_size>
     <<<1, 1>>>(thrust::raw_pointer_cast(d_in.data()), thrust::raw_pointer_cast(d_out.data()), cub::Sum{});
   REQUIRE(cudaSuccess == cudaPeekAtLastError());
   REQUIRE(cudaSuccess == cudaDeviceSynchronize());
   verify_results(reference_result, c2h::host_vector<int>(d_out)[0]);
-#endif // _CCCL_STD_VER >= 2023
+#  endif // _CCCL_STD_VER >= 2023
 }
+
+#endif // defined(CCCL_CHECK_SASS)
